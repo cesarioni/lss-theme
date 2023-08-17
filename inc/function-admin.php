d498fda1c208127c2d16e243abe7db2f05655cf2
cesarioni <cesarioni@hotmail.com>
2023-08-17 16:12:26 +0200

    test

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -11,8 +11,10 @@
 
 function lss_add_admin_page() {
 	
-	add_menu_page( 'LSS Theme Options', 'LSS', 'manage_options', 'cesar-lss', 'lss_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
-	add_submenu_page( 'cesar-lss', 'LSS Theme Options', 'General', 'manage_options', 'cesar_lss', 'lss_theme_create_page' );
+	add_menu_page( 'lss Theme Options', 'lss', 'manage_options', 'cesar-lss', 'lss_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110 );
+	add_submenu_page( 'cesar-lss', 'lss Theme Options', 'General', 'manage_options', 'cesar_lss', 'lss_theme_create_page' );
+	add_submenu_page( 'cesar-lss', 'lss CSS Options', 'Custom CSS', 'manage_options', 'cesar_lss_css', 'lss_theme_settings_page');
+	
 
 }
 add_action( 'admin_menu', 'lss_add_admin_page' );
